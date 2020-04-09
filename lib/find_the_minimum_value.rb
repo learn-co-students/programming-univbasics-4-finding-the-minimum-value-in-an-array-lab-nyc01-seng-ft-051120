66a2b17742699604bfd7c9333a4c6c327d79a4e3
Kevin Neyer <kevinneyer@gmail.com>
2020-04-09 18:49:20 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def find_min_value(array)
-  # Add your solution here
+ minimum = array.min
+ message = " "
+ 
+ array.length.times do |f| 
+   if array[f] <= minimum
+   message = array[f]
+   end
+ end
+    message  
 end
